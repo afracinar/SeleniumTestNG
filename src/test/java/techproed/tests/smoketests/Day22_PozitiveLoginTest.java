d10edca4be2493a1149716774d6a5aa7829fec1e
Afra Çınar <afracinaarr@gmail.com>
2023-02-18 11:18:15 +0300

    18.02.2023

--- src/test/java/techproed/tests/smoketests/Day22_PozitiveLoginTest.java
+++ src/test/java/techproed/tests/smoketests/Day22_PozitiveLoginTest.java
@@ -2,6 +2,7 @@ package techproed.tests.smoketests;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import techproed.pages.C05_BlueRentalHomePage;
 import techproed.pages.C06_BlueRentalLoginPage;
@@ -22,18 +23,23 @@ public class Day22_PozitiveLoginTest {
     C06_BlueRentalLoginPage c06_blueRentalLoginPage;
     @Test
     public void US100201_Admin_Login() throws InterruptedException {
-
+        Reporter.log("sayfaya git");
         Driver.getDriver().get(ConfigReader.getProperty("bluerentacar_url"));
 
         c05_blueRentalHomePage = new C05_BlueRentalHomePage();
+        Reporter.log("login butonuna tıkla");
+
         c05_blueRentalHomePage.loginButton.click();
 
         c06_blueRentalLoginPage = new C06_BlueRentalLoginPage();
+        Reporter.log("giriş bilgilerini gir");
+
         c06_blueRentalLoginPage.emailTextBox.sendKeys(ConfigReader.getProperty("admin_email"));
         Thread.sleep(5000);
         c06_blueRentalLoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("admin_password"));
         Thread.sleep(5000);
         c06_blueRentalLoginPage.loginButton.click();                             //giriş yaptıktan sonra isim yerinin locate'ini aldık
+        Reporter.log("giriş yapıldığını doğrula");
         Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//*[@id='dropdown-basic-button']")).isDisplayed());
     }
 }
